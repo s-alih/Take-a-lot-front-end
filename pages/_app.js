aa4e2ed2bb5592d31c4a290bf56d14bab5536342
s-alih <slh32064@gmail.com>
2022-08-09 23:36:37 +0530

    notification

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,14 @@
-import { MoralisProvider } from "react-moralis";
-import "../styles/globals.css";
-
+import { MoralisProvider } from "react-moralis"
+import "../styles/globals.css"
+import { NotificationProvider } from "web3uikit"
 function MyApp({ Component, pageProps }) {
-  return (
-    <MoralisProvider initializeOnMount={false}>
-      <Component {...pageProps} />
-    </MoralisProvider>
-  );
+    return (
+        <MoralisProvider initializeOnMount={false}>
+            <NotificationProvider>
+                <Component {...pageProps} />
+            </NotificationProvider>
+        </MoralisProvider>
+    )
 }
 
-export default MyApp;
+export default MyApp
